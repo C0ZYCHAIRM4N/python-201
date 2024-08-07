292c614af1065706803926f369f4f129d6dde2a0
Your Name <d1g1man129@gmail.com>
2024-08-07 11:12:07 -0700

    done

--- 02_more-datatypes/02_14_char_count_dict.py
+++ 02_more-datatypes/02_14_char_count_dict.py
@@ -4,3 +4,20 @@
 #
 # user_input = "hello"
 # result = {"h": 1, "e": 1, "l": 2, "o": 1}
+# Get input from the user
+user_input = input("Enter a string: ")
+
+# Create an empty dictionary to store the result
+result = {}
+
+# Iterate over the characters in the string
+for char in user_input:
+    # If the character is already in the dictionary, increment its value
+    if char in result:
+        result[char] += 1
+    else:
+        # If the character is not in the dictionary, add it with a value of 1
+        result[char] = 1
+
+# Print the result
+print(result)

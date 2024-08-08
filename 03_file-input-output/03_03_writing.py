4725f0829983b4272b604f06fe0e47a72d3ffe79
Your Name <d1g1man129@gmail.com>
2024-08-08 11:17:42 -0700

    done

--- 03_file-input-output/03_03_writing.py
+++ 03_file-input-output/03_03_writing.py
@@ -1,2 +1,8 @@
 # Write a script that reads in the contents of `words.txt`
 # and writes the contents in reverse to a new file `words_reverse.txt`.
+file_input = open(r"C:\Users\speng\OneDrive\Desktop\codingnomads 201 labs\python-201\03_file-input-output\words.txt", 'r')
+contents = file_input.read()
+reversed_content = contents[::-1]
+
+with open(r"C:\Users\speng\OneDrive\Desktop\codingnomads 201 labs\python-201\03_file-input-output\words_reverse.txt", 'w') as file_output:
+    file_output.write(reversed_contents)

c7cdfd1219699f0d1a6db3487c43f73a2fc9a084
Your Name <d1g1man129@gmail.com>
2024-08-08 09:50:04 -0700

    done

--- 03_file-input-output/03_01_long_words.py
+++ 03_file-input-output/03_01_long_words.py
@@ -1,2 +1,20 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
+
+# open file
+file_input = open(r"C:\Users\speng\OneDrive\Desktop\codingnomads 201 labs\python-201\03_file-input-output\words.txt", "r")
+
+
+for line in file_input:
+    words = line.split()
+    for word in words:
+        if len(word) > 20:
+            print(word)
+# Don't forget to close the file after you're done
+file_input.close()
+
+ 
+
+
+
+

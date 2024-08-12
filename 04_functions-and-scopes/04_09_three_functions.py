4134878f0c6c22dd70ff53ea7855e5dbf748f8f4
Your Name <d1g1man129@gmail.com>
2024-08-12 16:05:08 -0500

    done

--- 04_functions-and-scopes/04_09_three_functions.py
+++ 04_functions-and-scopes/04_09_three_functions.py
@@ -3,3 +3,12 @@
 # of that function to do something with it. You can have more
 # than three functions, and they don't need to call each other
 # in a circular way.
+def first_function():
+    return "Hello"
+def second_function():
+    example = first_function()
+    return example + " World"
+def third_function():
+    example2 = second_function()
+    return example2 + "!"
+print(third_function())

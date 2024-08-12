9b94bfbe9354f66a0d3a83524a30acacc9abe830
Your Name <d1g1man129@gmail.com>
2024-08-12 15:52:00 -0500

    done

--- 04_functions-and-scopes/04_07_determine_divisible.py
+++ 04_functions-and-scopes/04_07_determine_divisible.py
@@ -7,3 +7,16 @@
 # - call your functions, passing in the user input as the arguments,
 #   and set their output equal to new variables 
 # - print your the result variables with descriptive messages
+def divisible_by_four_or_seven(num):
+    return num % 4 == 0 or num % 7 == 0
+print(divisible_by_four_or_seven(8))
+
+def divisible_by_four_and_seven(num):
+    return num % 4 == 0 and num % 7 == 0
+print(divisible_by_four_and_seven(8))
+
+user_input = int(input("Enter a number between 1 and 1,000,000,000: "))
+result_one = divisible_by_four_or_seven(user_input)
+result_two = divisible_by_four_and_seven(user_input)
+print(f"Is {user_input} divisible by 4 or 7? {result_one}")
+print(f"Is {user_input} divisible by 4 and 7? {result_two}")

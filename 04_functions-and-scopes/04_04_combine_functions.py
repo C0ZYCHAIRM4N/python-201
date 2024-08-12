053635bb792de160c3a383c09e35d92bd2ca880e
Your Name <d1g1man129@gmail.com>
2024-08-12 15:44:03 -0500

    done

--- 04_functions-and-scopes/04_04_combine_functions.py
+++ 04_functions-and-scopes/04_04_combine_functions.py
@@ -2,3 +2,14 @@
 # with the `write_letter()` function from the previous exercise.
 # Write both functions in this script and call `greet()` within `write_letter()`
 # to let `greet()` take care of creating the greeting string.
+
+def greet(use):
+    return f"Hello {use}!"
+
+def write_letter(name, text):
+    greeting = greet(name)
+    goodbye = f"\n\nSincerely,\n{name}"
+    letter = greeting + text + goodbye
+    return letter
+letter = write_letter("Alice", "I hope this letter finds you well.")
+print(letter)

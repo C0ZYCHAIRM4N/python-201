3d516c528b5367990db3a4f782cd8e800cab3e4f
Your Name <d1g1man129@gmail.com>
2024-08-12 15:46:11 -0500

    done

--- 04_functions-and-scopes/04_06_sandwich_stand.py
+++ 04_functions-and-scopes/04_06_sandwich_stand.py
@@ -3,3 +3,10 @@
 # - takes an arbitrary amount of toppings
 # - returns a string representing a sandwich with the bread on top
 #   and bottom, and the toppings in between.
+def make_sandwich(bread, *toppings):
+    sandwich = f"{bread}\n"
+    for topping in toppings:
+        sandwich += f"{topping}\n"
+    sandwich += bread
+    return sandwich
+print(make_sandwich("Wheat", "Cheese", "Tomato", "Lettuce"))

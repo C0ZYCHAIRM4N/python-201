0b4e1df7827ef82a4d0a3593b47819f6ce18cf85
Your Name <d1g1man129@gmail.com>
2024-08-21 11:13:49 -0500

    done

--- 04_functions-and-scopes/04_11_your_realtor.py
+++ 04_functions-and-scopes/04_11_your_realtor.py
@@ -3,3 +3,14 @@
 # is why your function should be able to take an arbitrary amount of
 # keyword arguments, and display them all in a list form with some 
 # introductory information.
+def realtor(**kwargs):
+    print("New real estate advertisement:")
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+# Call the function with the following keyword arguments:
+# - `location`: "San Francisco"
+# - `price`: "$1,000,000"
+# - `rooms`: 3
+# - `bathrooms`: 2
+# - `pool`: True
+realtor(location="San Francisco", price="$1,000,000", rooms=3, bathrooms=2, pool=True)

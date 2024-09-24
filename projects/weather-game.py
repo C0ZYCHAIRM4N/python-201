59919213ccfb7d991658bc11c18b706773523c18
Your Name <d1g1man129@gmail.com>
2024-09-24 12:52:47 -0500

    done

--- projects/weather-game.py
+++ projects/weather-game.py
@@ -1,3 +1,27 @@
 # Include the current weather into your game mechanics.
 
 URL = "https://www.metaweather.com/api/"
+
+import requests
+
+def get_location_woeid(location):
+    url = f"https://www.metaweather.com/api/location/search/?query={location}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            return data[0]['woeid']
+        else:
+            return None
+    else:
+        return None
+
+def get_current_weather(woeid):
+    url = f"https://www.metaweather.com/api/location/{woeid}/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        weather = data['consolidated_weather'][0]
+        return weather['weather_state_name'], weather['the_temp']
+    else:
+        return None, None
